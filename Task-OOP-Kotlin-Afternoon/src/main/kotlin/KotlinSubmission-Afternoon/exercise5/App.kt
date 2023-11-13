4b9a88698f76da2737410c06fb4b7a01a0646f7a
KT21 <147123365+theodorusk@users.noreply.github.com>
2023-11-13 14:03:21 +0700

    Update App.kt

--- Task-OOP-Kotlin-Afternoon/src/main/kotlin/KotlinSubmission-Afternoon/exercise5/App.kt
+++ Task-OOP-Kotlin-Afternoon/src/main/kotlin/KotlinSubmission-Afternoon/exercise5/App.kt
@@ -77,3 +77,82 @@ fun main() {
 
 
 }
+package id.infinitelearning.KotlinSubmission.exercise5
+
+class hero {
+
+    private var name: String? = null
+    private var age = 0
+    private var height = 0
+    private var health = 20
+
+    fun setProfile(name: String?, age: Int, height: Int) {
+        this.name = name
+        this.age = age
+        this.height = height
+    }
+
+    fun profile() {
+        println("========= Profile =========")
+        println("Name: $name")
+        println("Age: $age")
+        println("Height: $height")
+        println("Health: $health")
+        println("===========================")
+    }
+
+    private fun checkHealth() {
+        if (health <= 0) {
+            println("Anda telah mati $name")
+        }
+    }
+
+    fun jalan() {
+        checkHealth()
+        println("$name sedang berjalan...")
+        health -= 1
+    }
+
+    fun lari() {
+        checkHealth()
+        println("$name sedang berlari...")
+        health -= 2
+    }
+
+    fun makan() {
+        checkHealth()
+        println("$name sedang makan...")
+        health += 2
+    }
+
+    fun minum() {
+        checkHealth()
+        println("$name sedang minum...")
+        health += 2
+    }
+
+    fun lompat() {
+        checkHealth()
+        println("$name sedang lompat...")
+        health -= 2
+    }
+
+    fun duduk() {
+        checkHealth()
+        println("$name sedang duduk...")
+        health += 1
+    }
+}
+
+fun main() {
+    val hero = Hero()
+    hero.setProfile("Theo", 22, 178)
+
+    hero.profile()
+    hero.lari()
+    hero.makan()
+    hero.minum()
+    hero.lompat()
+    hero.duduk()
+    hero.profile()
+}
